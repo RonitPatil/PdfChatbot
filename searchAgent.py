5b7baae7ce5cd4cf48fab6642ddb5d41752cb7d3
RonitPatil <83586482+RonitPatil@users.noreply.github.com>
2024-06-12 17:05:19 -0700

    Update searchAgent.py

--- searchAgent.py
+++ searchAgent.py
@@ -2,6 +2,7 @@ import streamlit as st
 from lyzr import ChatBot
 import os
 import tempfile
+from openai import OpenAI
 from utils import save_uploaded_file, remove_existing_files
 from dotenv import load_dotenv
 
